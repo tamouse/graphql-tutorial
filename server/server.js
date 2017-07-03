189125e8e12da7ef8dca4467891b7fa940ba13f8
Tamara Temple <tamara.temple@reachlocal.com>
2017-07-03 14:26:13 -0500

    tutorial 2 server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
+import {
+    graphqlExpress,
+    graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
+
+import { schema } from './src/schema';
+
 import express from 'express';
 
 const PORT = 4000;
 
 const server = express();
 
-server.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
 
 server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
---
+++ server/src/resolvers.js
@@ -0,0 +1,14 @@
+const channels = [{
+  id: 1,
+  name: 'soccer',
+}, {
+  id: 2,
+  name: 'baseball',
+}];
+export const resolvers = {
+  Query: {
+    channels: () => {
+      return channels;
+    },
+  },
+};
